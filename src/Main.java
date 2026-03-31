c2298b0864e235627ff02aa3b60a8faa0542908f
yagops0 <yagosalvador34@gmail.com>
2026-03-31 20:43:37 -0300

    v0.2.1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import controllers.TarefaController;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("================= AGENDA PESSOAL =================");
+        TarefaController tarefaController = new TarefaController();
+        tarefaController.processamento();
 
 
 
